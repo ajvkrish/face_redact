fdc2b7279e060377851ac44f0136779b479d8d11
Ajay Krishnaswamy <ajvkrish@gmail.com>
2022-06-28 22:14:25 -0400

    first commit

---
+++ src/faceblur.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+video = cv2.VideoCapture(0)
+
+while(True):
+    ret,frame=video.read()
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    roi=face.detectMultiScale(frame,1.3,5)
+    for(x,y,w,h) in roi:
+        frame[y:y+h,x:x+w]=cv2.medianBlur(frame[y:y+h,x:x+w],45)
+    cv2.imshow("window",frame)
+    key=cv2.waitKey(30)
+    if(key==27):
+        break
+video.release()
+cv2.destroyAllWindows()
+
